d4eb2db9813c20ac38b463d5f02a9e4582b37cd6
GeraldAkenji <akenjigerald@gmail.com>
2024-07-17 16:55:48 -0600

    modified commit

---
+++ variable.tf
@@ -0,0 +1,11 @@
+variable "my_bucket_region" {
+  description = "my defualt bucket region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "my_bucket_name" {
+  description = "my bucket name"
+  type        = string
+  default     = "demo-mybucket23"
+}
